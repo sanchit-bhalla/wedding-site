27df872852a24d2b655d8099d947050f438522ee
Sanchit Bhalla <46027254+sanchit-bhalla@users.noreply.github.com>
2025-12-22 03:50:30 +0530

    Comment out mehandi video ID in videoIdMap
    Comment out the 'mehandi' video ID mapping.

--- constants/index.ts
+++ constants/index.ts
@@ -17,7 +17,7 @@ export const CREATOR_TWITTER =
 export const videoIdMap: Record<string, string> = {
   // "bangles-ceremony": "FzhGJgBT7mo",
   "pre-wedding-shoot": "cfdNyHt04RE",
-  mehandi: "FzhGJgBT7mo",
+  //mehandi: "FzhGJgBT7m",
   // "sagan-and-reception": "FzhGJgBT7mo",
   "haldi-and-saint": "FzhGJgBT7mo",
   // wedding: "FzhGJgBT7mo",
